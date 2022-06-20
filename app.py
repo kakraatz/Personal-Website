7a367b9565deeadeef417d42eddb5ab3cbc0acf1
Kevin <86261021+kakraatz@users.noreply.github.com>
2022-06-20 11:13:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 """
-This module is used to start the Local Library Database Maintenance Flask
-application. It can be started by typing the following command into a terminal:
-
     Windows: python app.py
     MacOS: python3 app.py
 """
@@ -13,12 +10,12 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'kraatzk'
-app.config['MYSQL_PASSWORD'] = 'KodaDol01171993!!'
-app.config['MYSQL_DB'] = 'kraatzk'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
-app.config['SECRET_KEY'] = "Osucs2023!!"
+app.config['SECRET_KEY'] = ""
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 mysql = MySQL(app)
